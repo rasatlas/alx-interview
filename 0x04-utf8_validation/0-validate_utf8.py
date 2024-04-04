aeba074f12d74e75df88e05762d895caf492f5b6
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-04 19:07:42 +0300

    UTF-8 validator

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,10 +3,11 @@
 
 
 def validUTF8(data):
-    """ A method that determines if a given data set represents
+    """A method that determines if a given data set represents
     a valid UTF-8 encoding.
     """
-    i = 0
+
+    """ i = 0
     while i < len(data):
         # Count the number of bytes in the current character
         num_bytes = 0
@@ -26,4 +27,27 @@ def validUTF8(data):
 
         i += num_bytes
 
-    return True
+    return True """
+
+    num_bytes_to_follow = 0
+
+    for byte in data:
+        # Check if the byte is a continuation byte
+        if num_bytes_to_follow:
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes_to_follow -= 1
+        else:
+            # Determine the number of bytes to follow based on the leading bits
+            if byte >> 7 == 0:
+                continue  # Single-byte character
+            elif byte >> 5 == 0b110:
+                num_bytes_to_follow = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes_to_follow = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes_to_follow = 3
+            else:
+                return False  # Invalid leading bits
+
+    return num_bytes_to_follow == 0
